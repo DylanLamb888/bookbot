d31d4ce2e6b68e0189a633f3d384627c323e7ca6
DylanLamb888 <johndylanlamb@proton.me>
2025-09-29 21:23:43 +0100

    completed first project

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import count_words
 from stats import count_characters
 from stats import sort_character_counts
@@ -8,13 +10,18 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
+    book_text = get_book_text(book_path)
     word_count = count_words(book_text)
     char_counts = count_characters(book_text)
     sorted_chars = sort_character_counts(char_counts)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
