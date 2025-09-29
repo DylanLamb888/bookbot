272ba77de7e63e96dbe98b5b84c5d51a3438f271
DylanLamb888 <johndylanlamb@proton.me>
2025-09-29 18:35:13 +0100

    counted the words

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ def get_book_text(filepath):
         file_contents = f.read()
     return file_contents
 
+def count_words(text):
+    words = text.split()
+    return len(words)
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    num_words = count_words(book_text)
+    print(f"Found {num_words} total words")
 
 main()
 
