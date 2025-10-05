c0a3cc92ae264a51f17802571382349b030b94f5
DylanLamb888 <johndylanlamb@proton.me>
2025-10-05 08:38:01 +0100

    added stats file

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+from stats import get_num_words
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"Found {num_words} total words")
+
+
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
-
-def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    words = book_text.split()
-    print(f"Found {len(words)} total words.")
 
 
 main()
