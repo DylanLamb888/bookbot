c95cec83df4508e8be622691fa90a3bf05c1cabc
DylanLamb888 <johndylanlamb@proton.me>
2025-10-05 08:56:35 +0100

    added chars dict

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from stats import get_num_words
+from stats import get_chars_dict
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
     print(f"Found {num_words} total words")
+    print(chars_dict)
 
 
 def get_book_text(file_path):
