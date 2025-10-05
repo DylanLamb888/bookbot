ead461ac67bdc77b11370f447d22f8d099cc2c3c
DylanLamb888 <johndylanlamb@proton.me>
2025-10-05 11:38:33 +0100

    finsihed project

--- stats.py
+++ stats.py
@@ -12,5 +12,22 @@ def get_chars_dict(text):
             chars[lowered] = 1
     return chars
 
+def sort_on(dict):
+    return dict["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+
+
+
+
+
+
 
 
