c76e2250dbf8818ed008f6a63aa66149050d724e
DylanLamb888 <johndylanlamb@proton.me>
2025-10-22 10:30:22 +0100

    get book text function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    get_text = get_book_text("books/frankenstein.txt")
+    print(get_text)
+
+main()
