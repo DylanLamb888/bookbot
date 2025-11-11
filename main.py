0717685837af0eadc3a0511c3cddf88e051968df
DylanLamb888 <johndylanlamb@proton.me>
2025-11-11 22:59:52 +0000

    word count function added

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+from stats import count_words
+
 def main():
     book_text = "books/frankenstein.txt"
     text = get_book_text(book_text)
-    print(text)
-    
+    word_count = count_words(text)
+    print(f"Found {word_count} total words")
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
-    
+
 main()
---
+++ stats.py
@@ -0,0 +1,2 @@
+def count_words(book_str):
+    return len(book_str.split())
