71b857a1d8d662d059b543dc7bcc2e4674cab178
DylanLamb888 <johndylanlamb@proton.me>
2025-11-12 19:00:33 +0000

    full report print

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from stats import count_words
+from stats import count_words, count_characters, sorted_report
 
 def main():
     book_text = "books/frankenstein.txt"
     text = get_book_text(book_text)
     word_count = count_words(text)
+    character_counter = count_characters(text)
+    sorted_list = sorted_report(character_counter)
+    report_printing = print_report(book_text, word_count, sorted_list)
+    return report_printing
+
+
+def print_report(book_text, word_count, sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_text}...")
+    print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for char in sorted_list:
+        if not char["char"].isalpha():
+            continue
+        print(f"{char["char"]}: {char["num"]}")
+    print("============= END ===============")
 
 def get_book_text(filepath):
     with open(filepath) as f:
--- stats.py
+++ stats.py
@@ -1,2 +1,29 @@
 def count_words(book_str):
     return len(book_str.split())
+
+def count_characters(book_text):
+    char_dict = {}
+    for char in book_text:
+        lowered = char.lower()
+        if lowered in char_dict:
+            char_dict[lowered] += 1
+        else:
+            char_dict[lowered] = 1
+    return char_dict
+
+def sort_on(items):
+    return items["num"]
+
+
+def sorted_report(char_dict):
+    sorted_list = []
+    for k in char_dict:
+        v = char_dict[k]
+        new_dict = {
+            "char"  : k,
+            "num"   : v
+        }
+        sorted_list.append(new_dict)
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
