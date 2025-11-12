ec9f00f9ab80ae20d43c1ccf21906bff7c22605c
DylanLamb888 <johndylanlamb@proton.me>
2025-11-12 20:43:11 +0000

    added sys argv

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from stats import count_words, count_characters, sorted_report
+import sys
 
 def main():
-    book_text = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_text = sys.argv[1]
     text = get_book_text(book_text)
     word_count = count_words(text)
     character_counter = count_characters(text)
     sorted_list = sorted_report(character_counter)
-    report_printing = print_report(book_text, word_count, sorted_list)
-    return report_printing
-
+    print_report(book_text, word_count, sorted_list)
+    
 
 def print_report(book_text, word_count, sorted_list):
     print("============ BOOKBOT ============")
